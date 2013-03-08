a0a733e87aca3d946535b936e47b16f08be550b6
Jonas Gehring <jonas.gehring@inovex.de>
2013-03-08 15:21:34 +0100

    canvas player +websocket connection

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var io = require('socket.io').listen(8088);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
